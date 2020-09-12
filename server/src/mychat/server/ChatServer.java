d4cedccdf007e066a7ed9f2a0fe437bdef1a9a60
isaev_k <64546763+retardUnicorn@users.noreply.github.com>
2020-09-12 20:12:03 +0900

    Add files via upload

---
+++ server/src/mychat/server/ChatServer.java
@@ -0,0 +1,67 @@
+package mychat.server;
+
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
+import mychat.network.TCPConnection;
+import mychat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.ServiceConfigurationError;
+
+public class ChatServer implements TCPConnectionListener {
+
+    public static void main(String[] args) {
+        new ChatServer();
+
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer(){
+        System.out.println("Server running...");
+        try(ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                   new TCPConnection(this, serverSocket.accept());
+
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("Exception: " + e);
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        final int cnt = connections.size();
+        for (int i = 0; i < connections.size(); i++) {
+            connections.get(i).sendstring(value);
+        }
+    }
+}
